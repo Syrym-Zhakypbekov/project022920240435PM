3f1568d87bb7cb88cccf53cd1f4ec87b09454cda
Syrym Zhakypbekov <43728607+Syrym-Zhakypbekov@users.noreply.github.com>
2024-02-29 16:42:47 +0600

    test
    test

---
+++ src/main/java/com/mygroup/project022920240435pm/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package com.mygroup.project022920240435pm.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
